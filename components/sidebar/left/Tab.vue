d73c3f5d79d3a43aac00859ed88c99d3d79a5d14
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-01-24 15:48:38 +0100

    SiderbarLeft component updated

---
+++ components/sidebar/left/Tab.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const textWeight = computed(() => ({
+  'font-semibold': props.active,
+  'font-normal': !props.active,
+}))
+
+const textColor = computed(() => ({
+  'text-slate-700': props.active,
+  'text-slate-500': !props.active,
+  'dark:text-slate-100': props.active,
+  'dark:text-slate-400': !props.active,
+}))
+</script>
+
+<template>
+  <NuxtLink
+    to="/"
+    class="flex w-min items-center gap-4 rounded-full p-3 transition hover:bg-gray-100 dark:hover:bg-slate-700"
+    :class="textColor"
+  >
+    <slot name="icon" />
+    <span class="hidden text-xl xl:block" :class="textWeight">
+      <slot name="label" />
+    </span>
+  </NuxtLink>
+</template>
--- components/sidebar/left/index.vue
+++ components/sidebar/left/index.vue
@@ -5,9 +5,61 @@
     <div class="sticky top-0">
       <TheLogo />
 
-      <NuxtLink to="/">
-        <Icon name="heroicons:home-20-solid" size="24px" />
-      </NuxtLink>
+      <SidebarLeftTab active>
+        <template #icon>
+          <Icon name="heroicons:home-solid" size="24px" />
+        </template>
+        <template #label>Home</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:hashtag-solid" size="24px" />
+        </template>
+        <template #label>Explore</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:bell-solid" size="24px" />
+        </template>
+        <template #label>Notifications</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:inbox-solid" size="24px" />
+        </template>
+        <template #label>Messages</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:bookmark-solid" size="24px" />
+        </template>
+        <template #label>Bookmarks</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:document-text-solid" size="24px" />
+        </template>
+        <template #label>Lists</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:user-circle-solid" size="24px" />
+        </template>
+        <template #label>Profile</template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template #icon>
+          <Icon name="heroicons:ellipsis-horizontal-circle-solid" size="24px" />
+        </template>
+        <template #label>More</template>
+      </SidebarLeftTab>
     </div>
   </div>
 </template>
